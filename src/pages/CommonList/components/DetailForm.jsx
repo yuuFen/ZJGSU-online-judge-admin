85a8c9ee7511b4212980061314203ae3109f4683
Yu <me@yuufen.com>
2020-07-15 21:43:30 +0800

    新建 DetailForm

---
+++ src/pages/CommonList/components/DetailForm.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Modal } from 'antd';
+
+const DetailForm = (props) => {
+  const { modalVisible, onCancel } = props;
+  return (
+    <Modal
+      destroyOnClose
+      title={props.title}
+      visible={modalVisible}
+      onCancel={() => onCancel()}
+      footer={null}
+    >
+      {props.children}
+    </Modal>
+  );
+};
+
+export default DetailForm;
